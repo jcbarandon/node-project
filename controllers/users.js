38d72668e328e4cc18ab80583c32d08eb548f536
jcbarandon <jcbarandon@users.noreply.github.com>
2026-07-11 14:36:36 +0800

    Fix CRUD handlers: send responses, add status codes and validation

--- controllers/users.js
+++ controllers/users.js
@@ -3,34 +3,58 @@ import { v4 as uuid } from 'uuid';
 let users = [];
 
 export const getUsers = (req, res) => {
-    console.log(`Users in the database: ${users}`);
+    res.status(200).json(users);
+};
 
-    res.send(users);
-}
+export const createUser = (req, res) => {
+    const { username, age } = req.body;
 
-export const createUser = (req, res) => {   
-    const user = req.body;
+    if (!username || age === undefined) {
+        return res.status(400).json({ message: 'username and age are required' });
+    }
 
-    users.push({...user, id: uuid()});
-    
-    console.log(`User [${user.username}] added to the database.`);
+    const user = { id: uuid(), username, age };
+    users.push(user);
+
+    console.log(`User [${username}] added to the database.`);
+    res.status(201).json(user);
 };
 
 export const getUser = (req, res) => {
-    res.send(req.params.id)
-};
-
-export const deleteUser = (req, res) => { 
-    console.log(`user with id ${req.params.id} has been deleted`);
-    
-    users = users.filter((user) => user.id !== req.params.id);
-};
-
-export const updateUser =  (req,res) => {
     const user = users.find((user) => user.id === req.params.id);
-    
-    user.username = req.body.username;
-    user.age = req.body.age;
 
-    console.log(`username has been updated to ${req.body.username}.age has been updated to ${req.body.age}`)
+    if (!user) {
+        return res.status(404).json({ message: `User with id ${req.params.id} not found` });
+    }
+
+    res.status(200).json(user);
+};
+
+export const deleteUser = (req, res) => {
+    const user = users.find((user) => user.id === req.params.id);
+
+    if (!user) {
+        return res.status(404).json({ message: `User with id ${req.params.id} not found` });
+    }
+
+    users = users.filter((user) => user.id !== req.params.id);
+
+    console.log(`User with id ${req.params.id} has been deleted.`);
+    res.status(200).json({ message: `User with id ${req.params.id} deleted successfully` });
+};
+
+export const updateUser = (req, res) => {
+    const user = users.find((user) => user.id === req.params.id);
+
+    if (!user) {
+        return res.status(404).json({ message: `User with id ${req.params.id} not found` });
+    }
+
+    const { username, age } = req.body;
+
+    if (username) user.username = username;
+    if (age !== undefined) user.age = age;
+
+    console.log(`User with id ${req.params.id} has been updated.`);
+    res.status(200).json(user);
 };
